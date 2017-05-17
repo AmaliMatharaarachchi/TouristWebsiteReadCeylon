59d438a8f94f197b28e15c656f604ddd135e60c9
Amali Lakshika <maamalilakshika@gmail.com>
2017-05-17 14:48:26 +0530

    Added a Procfile.

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -10,9 +10,14 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+
 class UnitTest extends TestCase
 {
     use DatabaseTransactions;
+
+    public function testPageWelcome(){
+        $this->visit('/')->see('ReadCeylon');
+    }
 
     public function test_create_package()
     {
